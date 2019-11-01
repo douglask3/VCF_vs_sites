2ee3b43c20936a645fa6fa948136e0ed32259a31
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-11-01 18:39:31 +0100

    moving some functions into their own library file

--- VCF_histogram_machine_28.8.r
+++ VCF_histogram_machine_28.8.r
@@ -1,8 +1,9 @@
 library(reldist)
+source("libs/make_col_vector.r")
+
 ##################
 ## define stuff ##
 ##################
-#setwd("C:/Users/Rahayu Adzhar/Desktop/30.7")
 graphics.off()
 height_cover_file = 'data/plotcover-23.08.csv'
 vcf_cover_file    = 'data/VCF_data_annual_means_21.8.csv'
@@ -57,50 +58,6 @@ vcf_covers[, 'veg_type'] = t(veg_type)
 
 ## Just to keep track of things, we going to assign each site a colour and stick that in 
 ## the vcf_cover data.frame, were we're storing a lot of the met info.
-
-make_col_vector <- function(r,g,b,ncols,
-							limits = NULL, whiteAt0 = TRUE) {    
-	if (!is.null(limits)) {
-		ncols = length(limits+1)
-		if (whiteAt0 && class(r)=="character") {
-			whiteIndex = which(r == "white" | r == "#FFFFFF")
-			if (length(whiteIndex) == 0)  whiteIndex = which(r == "black" | r == "#000000")
-			if (length(whiteIndex) != 0) {
-				zeroIndex  = which(limits[-1]>0 & head(limits,-1)<0)
-
-				if ( length(zeroIndex)==0) {
-					if (limits[1] > 0)r = r[whiteIndex:length(r)]
-						else r = r[1: whiteIndex]
-
-					return(make_col_vector_gubbins(r,ncols = length(limits)))
-				}
-
-				negCols = make_col_vector_gubbins(r[1:whiteIndex],
-												  ncols = zeroIndex+1)
-				posCols = make_col_vector_gubbins(r[whiteIndex:length(r)],
-												  ncols = length(limits) - zeroIndex + 1)
-				return(c(negCols,posCols[-1]))
-				}
-			}
-	}
-	return(make_col_vector_gubbins(r, g, b, ncols) )
-}
-
-make_col_vector_gubbins <- function(r, g, b, ncols) {
-    library(colorspace)
-	if (class(r)=="character") {
-		col=col2rgb(r)
-		r=col[1,]/255
-		g=col[2,]/255
-		b=col[3,]/255
-	}
-
-	col_vec <- function(a) approx(seq(1,ncols,length.out=length(a)),a,1:ncols)$y
-
-	cols=colorspace::RGB(col_vec(r),col_vec(g),col_vec(b))
-
-	return(hex(cols))
-}
 
 vcf_covers[, 'cols'] = 'black'
 for (type in names(cols)) {
