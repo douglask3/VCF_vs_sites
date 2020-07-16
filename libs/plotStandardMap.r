0f36d776140b5d924dc5b4cf25528cc1633780f4
Douglas Kelley <douglas.i.kelley@gmail.com>
2020-07-16 17:58:26 +0100

    pltting out transofmration maps, hostograms, and calultaing +ive anbd i-ve chane for each LU type

---
+++ libs/plotStandardMap.r
@@ -0,0 +1,104 @@
+source("../gitProjectExtras/gitBasedProjects/R/sourceAllLibs.r")
+sourceAllLibs('../rasterextrafuns/rasterPlotFunctions/R/')
+source("libs/return_multiple_from_functions.r")
+source("libs/sd.raster.r")
+
+library(raster)
+
+library(plotrix)
+library(mapdata)
+library(mapplots)
+
+library(rgdal)
+
+#SA_ste <- readOGR(dsn = "data/South_America", layer = "South_America")
+rivers <- readOGR(dsn = "data/majorrivers_0_0", layer = "MajorRivers")
+
+StandardLegend <- function(cols, limits, dat, rightx = 0.95, extend_max = TRUE, oneSideLabels = NA, add = FALSE, ...) {
+    if (add)        
+        plot_loc = c(0.41, rightx, 0.1, 0.13)
+    else 
+        plot_loc = c(0.05, rightx, 0.3, 0.56)
+    add_raster_legend2(cols, limits, dat = dat, add = add,
+                       transpose = FALSE, srt = 0, oneSideLabels= oneSideLabels,
+                       plot_loc = plot_loc,
+                       ylabposScling = 1, extend_max = extend_max, ...)
+}
+
+lineBox <- function(x, y, ...) 
+    lines(c(x[1], x[2], x[2], x[1], x[1]), c(y[1], y[1], y[2], y[2], y[1]),...)
+
+plotStandardMap <- function(r, cols, limits, e = NULL, add_legend = FALSE,
+                            limits_error = c(0.5, 0.500000001),
+                            title2 = '', title3 = '', txt.col = "black",
+                            xlim = c(-120, 160), ylim = c(-30, 30),...) {
+     
+    if (nlayers(r) > 1 && is.null(e)) {
+        if (nlayers(r) == 3) {
+            if (any(r[] <0, na.rm = TRUE) && any(r[] > 0, na.rm = TRUE)) {
+                e = r[[2]]
+                #e[!is.na(r[[2]])] = 0
+                e = abs(r[[3]] - r[[1]])/max(abs(r[[c(1,3)]]))/2
+                e[r[[3]]>0 & r[[1]] <0] = 1                
+            } else  e = 1-r[[1]]/r[[3]]
+            
+            r = r[[2]]
+        } else {
+            e = sd.raster(r)
+            r = mean(r)
+        }
+    } 
+    
+    r[r>9E9] = NaN
+    if (!is.null(e)) e[is.na(r)] = NaN
+    
+    plot(xlim, ylim, xlab = '', ylab = '', axes = FALSE, type ='n', xaxs = 'i', yaxs = 'i')
+    grid()
+    plot_raster_from_raster(r, e = e,interior = FALSE,#coast.lwd = NULL,
+                            cols = cols, limits = limits, add_legend = FALSE,
+                            quick = TRUE, ePatternRes = 60, ePatternThick = 0.87,
+                            limits_error = limits_error, add = TRUE, ...)
+    
+    #plot(rivers, col = c(rep("#FFFFFF00", 9), "black", rep("#FFFFFF00", 88)), add = TRUE, lwd = 2.5)
+    #plot(rivers, col = c(rep("#FFFFFF00", 9), "white", rep("#FFFFFF00", 88)), add = TRUE, lwd = 0.5)
+    #lines(SA_ste)
+    #lines(rivers, col = "white", lwd = 0.67)
+    #lines(SA_ste, lty = 2)
+    #lineBox(-c(71.25, 63.75), -c(11.25,  6.25))     
+    #lineBox(-c(61.25, 53.75), -c(11.25,  6.25))   
+    #lineBox(-c(48.25, 43.25), -c( 8.75,  1.25))
+    #lineBox(-c(66.25, 58.75), -c(18.75, 13.75))
+    addCoastline()
+    polygon(c(-62.5, -35, -35, -62.5), c(-56, -56, -50, -50), border = NA, col = "white")
+    mtext(title3, adj = 0.01, line = 0, col = txt.col)
+    mtext(title2, side = 2, line = -0.75, col = txt.col)
+    if (add_legend) {
+        add_raster_legend2(cols, limits, dat = r,
+                           transpose = FALSE, srt = 0, oneSideLabels= FALSE,
+                           plot_loc = c(0.35, 0.99, 0.09, 0.12),  ylabposScling=0.8, ...)
+    }
+}
+
+addCoastline <- function() {
+    
+    mask = raster('data/seamask.nc')
+    mask = mask>1
+    
+    plot_raster_from_raster(mask+1, add = TRUE, 
+                             cols = c("white", "transparent"),readyCut = TRUE,
+                             limits =  NULL, quick = TRUE, interior = FALSE, 
+                             coast.lwd = NULL, add_legend = FALSE)
+    #
+    #contour(mask, add = TRUE, drawlabels = FALSE, lwd = 0.5)  
+
+    ployBox <- function(x, y)
+        polygon(c(x[1], x[2], x[2], x[1]), c(y[1], y[1], y[2], y[2]), col = "white", border = "white")
+        
+    ployBox(c(-180, -90), c(-60, 0))
+    ployBox(c(-180, -120), c(-60, 25))
+    ployBox(c(-50, -19), c(10, 25))
+    ployBox(c(-50, -13.5), c(27.5, 34))
+    ployBox(c(115, 125), c(-8, -7))
+    ployBox(c(104, 111), c(2.5, 8))
+    ployBox(c(122, 128), c(2.5, 5)) 
+}
--- plot_gridded_experiments.r
+++ plot_gridded_experiments.r
@@ -1,114 +1,171 @@
 source('libs/plotStandardMap.r')
 source("../LimFIRE/src/libs/biomeInfo.r")
 library(rasterExtras)
+library(gitBasedProjects)
 graphics.off()
 
-teow = '../LimFIRE/data/official_teow/official/'
+#teow/ = '../LimFIRE/data/official_teow/official/'
 cols = c('#ffffe5','#f7fcb9','#d9f0a3','#addd8e','#78c679','#41ab5d','#238443','#006837','#004529')
 limits = c(0, 1, 2, 5, 10, 20, 50)
 
 dcols = c('#40004b','#762a83','#9970ab','#c2a5cf','#f7f7f7','#a6dba0','#5aae61','#1b7837','#00441b')
-dlimits = c(-35, -30, -25, -20, -15, -10, -5, -2, -1, 1, 2, 5, 10, 15, 20, 25, 30, 35)
-
-load("outputs/gridded_VCF_correction.Rd")
-
-output.csv <- function(r, name) {
-    ri = r[[1]]
-    x = cbind(xyFromCell(ri, 1:length(ri)), r[])
-    x = x[!mask[],]
-    colnames = c("lon", "lat")
-    if (ncol(x) == 3) colnames = c(colnames, "value")
-        else colnames = c(colnames, "5%", "50%", "95%")
-    colnames(x) = colnames
-    fname = paste0("outputs/", name, ".csv")
-    write.csv(x, file = fname, row.names = FALSE)
+dlimits = c(-20, -15, -10, -5, -2, -1, 1, 2, 5, 10, 15, 20)
+vegTypeNames = c("EG NL Forest", "EG BL Forest", "Dec NL Forest",
+                    "Dec BL Forest", "Mixed Forest",
+                    "Close Shrub", "Open Shrub", "Woody Savanna", "Savannas", "Grassland")
+forMask <- function(id) {
+    load(paste0("outputs/gridded_VCF_correction-stan-test2-mask", id, ".Rd"))
+    ### histogram
     
+    conHist = exps[[1]][-(1:13)][1:10]
+    expHist = lapply(exps, function(i) i[-(1:13)][11:20])
+    expHist = lapply(1:length(expHist[[1]]), function(i) lapply(expHist, function(j) j[[i]]))
+    browser()
+    lty_area = unlist(exps[[1]][4:13])
+    plotLUtype <- function(con, exp, name) {
+        plot(c(0, 100), c(0, 1), yaxt = 'n', xlab = '', cex = 1000,
+             pch = 19, col = "white", xaxs = 'i')
+        mtext(side = 3, line = -1.5, adj = 0.9, name)
+        
+        x = seq(0.5, 99.5, 1)
+        addPoly <- function(y, col = "black") {
+            test = y > 0
+            y = y[test]/max(y)
+            x = x[test]
+            polygon(c(x, rev(x)), c(y, rep(0, length(y))), border = col,
+                   col = make.transparent(col, 0.67))
+        }
+        addPoly(con)
+        mapply(addPoly, exp, c("red", "green", "blue", "pink"))
+        #browser()
+    }
+    if (id == 1) {
+        png("figs/vegTypesX.png", width = 7.2, height = 7.2*5/3, res = 300, units = 'in')
+            par(mfrow = c(5, 2), mar = rep(0.5, 4), oma = c(3,3,0, 0))
+        
+            mapply(plotLUtype, conHist, expHist,
+                  vegTypeNames)
+        
+        dev.off()
+    }
+    out = pout = nout = c()
+    mask = !is.na(do.call('sum', c(control, lapply(exps, function(i) i[[1]]))))
+    output.csv <- function(r, name) {
+        ri = r[[1]]
+       
+        
+        if (is.null(out)) {
+            out = cbind(xyFromCell(ri, 1:length(ri)), ri[])
+            out = out[mask[],]
+            colnames(out) = c("lon", "lat", "control")
+        } else {
+            x = ri[mask] + out[,3]
+            colnames(x) = paste0(name, '-',c("10%", "50%", "90%"))
+            out = cbind(out, x)
+        }
+        out <<- out
+        #if (ncol(x) == 3) colnames = c(colnames, "value")
+        #    else colnames = c(colnames, "5%", "50%", "95%")
+        #colnames(x) = colnames
+        #fname = paste0("outputs/", name, "mask", id, ".csv")
+        #
+
+        if (is.list(r) && length(r) >= 3) {
+            outNegPos <- function(i, x0) {
+                #fname = paste0("outputs/", "landTypeChange-", name, "-",
+                #                pname, "mask", id, ".csv")
+                x = r[[i + 1]][[1]]
+                
+                if (id == 2) x = x*250*250/(1000*1000*1000000)
+                else x = sweep(x, 2, unlist(r[4:8]), '/')
+                colnames(x) = vegTypeNames
+                rownames(x) = paste0(name, '-',rownames(x))
+                
+                return(rbind(x0, x))
+                #write.csv(x, file = fname)
+            } 
+            nout <<- outNegPos(1, nout)
+            pout <<- outNegPos(2, pout)
+        }
+    
+    }
+    titles = c('clumping0_overlap0', 'clumpingMax_overlap0',
+               'clumping0_overlapMax', 'clumpingMax_overlapMax')
+
+    output.csv(control, 'control')
+    mapply(output.csv, exps, titles)
+    
+    write.csv(out, file = paste0('outputs/output4histogram-mask_', id, '-git_rev_', gitVersionNumber(), '.csv'), row.names = FALSE)
+    
+    write.csv(rbind(nout, lty_area), file = paste0('outputs/output_negetativeChange-mask_', id, '-git_rev_', gitVersionNumber(), '.csv'), row.names = TRUE)
+    write.csv(rbind(pout, lty_area), file = paste0('outputs/output_positiveChange-mask_'  , id, '-git_rev_', gitVersionNumber(), '.csv'), row.names = TRUE)
+    
+    files = list.files("outputs/", pattern = "stan")
+    titles = c('no clumping/overlap', 'Max. clumping', 'Max. overlap', 'Max. clumping/overlap')
+    txt.cols = sapply(files[grepl('All', files)],
+                      function(i) strsplit(strsplit(i, 'All-')[[1]][2], '.csv')[[1]])
+    
+    #control[is.na(biomeAssigned)] = NaN
+    plotMap <- function(r, ...) {
+        r0 = r
+    
+        plotStandardMap(r, ...)
+        #contour(savanna, levels = 0.5, drawlabels = FALSE, add = TRUE)
+        
+        return(r)
+    }
+
+    png(paste0("figs/VCF_maps-mask",id,".png"),
+        height = 3.7, width = 7.2, units = 'in', res = 300)
+        layout(rbind(c(1, 0), c(2, 0), c(3, 4), c(5, 6), 7), heights = c(1, 0.45, 1, 1, 0.45))
+        par(mar = c(0, 0, 1.1, 0))
+        
+        plotMap(100*control, cols = cols, limits = limits, title3 = 'VCF')
+        par(mar = c(0, 0, 0.33, 0))
+            StandardLegend(cols, limits, 100*control, extend_max = FALSE, maxLab = 100, units = '%')
+        par(mar = c(0, 0, 1.1, 0)) 
+        exps = lapply(exps, function(i) 100*i[[1]])# (i - control) *100)
+        ehist = mapply(plotMap, exps, title3 = titles, txt.col = txt.cols,
+            MoreArgs = list(cols = dcols, limits = dlimits))
+        control = control*100       
+        ehist = lapply(ehist, function(i) i + control)
+        par(mar = c(0, 0, 0.33, 0))
+        StandardLegend(dcols, dlimits, exps[[2]], extend_min = TRUE, units = '%')
+    dev.off()
+    breaks = seq(0, 130, 1)
+
+    getHist <- function(r)   { 
+        if (nlayers(r) == 3) r = r[[2]]
+        # r = raster::crop(r, c(-20, 50, -30, 30)) 
+        out = hist(r[][r[] > 9], plot = FALSE, breaks = breaks)$counts
+        out = out#/max(out)
+    }
+
+    chist = getHist(control)
+    ehist = lapply(ehist, getHist)
+
+    my = max(chist, unlist(ehist))
+
+    png("figs/hist.png", height = 5, width = 5, res = 300, units = 'in')
+        plot(c(10, 100),c(0, my), type = 'n', yaxt = 'n', xaxs = 'i', xlab = '', ylab = '')
+        mtext('VCF cover (%)', side = 1, line = 2)
+
+        x = breaks[-1] + diff(breaks)/2
+        polyFun <- function(y, line = TRUE, fill = FALSE, ...) {
+            y = smooth.spline(x, y, spar=0.35)[[2]]#predict(loess(y~x))
+            if (line) lines(x, y, ...)
+            if (fill) polygon(c(x, rev(x)), c(y, rep(0, length(x))), border = NA, ...)
+        }
+        polyFun(chist, col = "grey", fill = TRUE, line = FALSE)
+        mapply(polyFun, ehist, col = make.transparent(txt.cols, 0.0), fill = FALSE, lwd = 2)
+    dev.off()
+
+    png("figs/hist0.png", height = 5, width = 5, res = 300, units = 'in')
+        plot(c(10, 100),c(0, my), type = 'n', yaxt = 'n', xaxs = 'i', xlab = '', ylab = '')
+        mtext('VCF cover (%)', side = 1, line = 2)
+    polyFun(chist, col = "black", fill = TRUE, line = FALSE)
+    dev.off()
 }
-titles = c('clumping0_overlap0', 'clumpingMax_overlap0',
-           'clumping0_overlapMax', 'clumpingMax_overlapMax')
 
-mask = is.na(sum(do.call(addLayer, exps)) + control)
-output.csv(control, 'control')
-mapply(output.csv, exps, titles)
-browser()
-
-if (T) {
-teow = readOGR(dsn = teow, layer = "wwf_terr_ecos")
-ext  = extent (-180, 180, -90, 90)
-xy   = abs(apply(as.matrix(bbox(ext)), 1, diff))
-n    = 2
-r    = raster(ext, ncol=xy[1]*n, nrow=xy[2]*n)
-
-## Rasterize the shapefile
-biome = rasterize(teow, r, 'BIOME')
-biome = raster::resample(biome, control)
-biomeAssigned = biome
-biomeAssigned[] = NaN
-for (i in 2:length(biomes))
-    biomeAssigned[any(layer.apply(biomes[[i]], function(j) biome == j))] = i
-
-savanna = (biomeAssigned > 2) & (biomeAssigned < 6)
-}
-
-files = list.files("outputs/")
-titles = c('no clumping/overlap', 'Max. clumping', 'Max. overlap', 'Max. clumping/overlap')
-txt.cols = sapply(files[grepl('Savanna', files)],
-                      function(i) strsplit(strsplit(i, 'Savanna-')[[1]][2], '.csv')[[1]])
-control[is.na(biomeAssigned)] = NaN
-plotMap <- function(r, ...) {
-    r0 = r
-    r[!savanna] = NaN
-    plotStandardMap(r, ...)
-    contour(savanna, levels = 0.5, drawlabels = FALSE, add = TRUE)
-    r[!savanna] = 0
-    return(r)
-}
-png("figs/VCF_maps.png", height = 3.7, width = 7.2, units = 'in', res = 300)
-    layout(rbind(c(1, 0), c(2, 0), c(3, 4), c(5, 6), 7), heights = c(1, 0.45, 1, 1, 0.45))
-    par(mar = c(0, 0, 1.1, 0))
-    plotMap(100*control, cols = cols, limits = limits, title3 = 'VCF')
-    par(mar = c(0, 0, 0.33, 0))
-        StandardLegend(cols, limits, 100*control, extend_max = FALSE, maxLab = 100, units = '%')
-    par(mar = c(0, 0, 1.1, 0)) 
-    exps = lapply(exps, function(i) (i - control) *100)
-    ehist = mapply(plotMap, exps, title3 = titles, txt.col = txt.cols,
-           MoreArgs = list(cols = dcols, limits = dlimits))
-    control = control*100       
-    ehist = lapply(ehist, function(i) i + control)
-    par(mar = c(0, 0, 0.33, 0))
-    StandardLegend(dcols, dlimits, exps[[2]], extend_min = TRUE, units = '%')
-dev.off()
-breaks = seq(0, 130, 1)
-
-getHist <- function(r)   { 
-    if (nlayers(r) == 3) r = r[[2]]
-   # r = raster::crop(r, c(-20, 50, -30, 30)) 
-    out = hist(r[][r[] > 9], plot = FALSE, breaks = breaks)$counts
-    out = out#/max(out)
-}
-
-chist = getHist(control)
-ehist = lapply(ehist, getHist)
-
-my = max(chist, unlist(ehist))
-
-png("figs/hist.png", height = 5, width = 5, res = 300, units = 'in')
-plot(c(10, 100),c(0, my), type = 'n', yaxt = 'n', xaxs = 'i', xlab = '', ylab = '')
-mtext('VCF cover (%)', side = 1, line = 2)
-
-x = breaks[-1] + diff(breaks)/2
-polyFun <- function(y, line = TRUE, fill = FALSE, ...) {
-    y = smooth.spline(x, y, spar=0.35)[[2]]#predict(loess(y~x))
-    if (line) lines(x, y, ...)
-    if (fill) polygon(c(x, rev(x)), c(y, rep(0, length(x))), border = NA, ...)
-}
-polyFun(chist, col = "grey", fill = TRUE, line = FALSE)
-mapply(polyFun, ehist, col = make.transparent(txt.cols, 0.0), fill = FALSE, lwd = 2)
-dev.off()
-
-png("figs/hist0.png", height = 5, width = 5, res = 300, units = 'in')
-plot(c(10, 100),c(0, my), type = 'n', yaxt = 'n', xaxs = 'i', xlab = '', ylab = '')
-mtext('VCF cover (%)', side = 1, line = 2)
-polyFun(chist, col = "black", fill = TRUE, line = FALSE)
-dev.off()
-
+forMask(1)
+forMask(2)
